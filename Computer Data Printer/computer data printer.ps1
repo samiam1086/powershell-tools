55032ce011cf06643207693dde637a142d7967a4
Daniel <57763139+samiam1086@users.noreply.github.com>
2020-09-15 23:08:57 -0400

    Add files via upload
    This program simply prints data about the computer to a file on the desktop

---
+++ Computer Data Printer/computer data printer.ps1
@@ -0,0 +1,13 @@
+﻿New-Item "$home\Desktop\out.txt"
+'Date' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+Get-Date | Out-File -FilePath "$home\Desktop\out.txt" -Append
+'Computer Info' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+Get-ComputerInfo | Out-File -FilePath "$home\Desktop\out.txt" -Append
+'Dns Client' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+Get-DnsClient | Out-File -FilePath "$home\Desktop\out.txt" -Append
+'Dns client cache' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+Get-DnsClientCache | Out-File -FilePath "$home\Desktop\out.txt" -Append
+'Powershell History' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+Get-History | Out-File -FilePath "$home\Desktop\out.txt" -Append
+'env: variables' | Out-File -FilePath "$home\Desktop\out.txt" -Append
+dir env: | Out-File -FilePath "$home\Desktop\out.txt" -Append
